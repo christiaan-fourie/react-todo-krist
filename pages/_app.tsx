cf5d055cb4e07bd6b4f73541d990f878763309c4
Christiaan Fourie <christiaanfourie60@gmail.com>
2022-10-06 10:33:49 +0200

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,18 @@
 import '../styles/tailwind.css';
 import '../styles/globals.css';
+import Head from 'next/head'
+
 
 import type { AppProps } from 'next/app';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <div className='h-screen bg-gray-900 text-gray-100'>
+      <Head>
+        <title>Simple To Do App</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="shortcut icon" href="favicon.png" type="image/x-icon"></link>
+      </Head>
       <Component {...pageProps} />
     </div>
   );  
