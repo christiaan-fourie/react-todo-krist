0a133dcad85d29f37b9ac9a1386840713ff9bf7e
Christiaan Fourie <christiaanfourie60@gmail.com>
2022-10-06 10:38:36 +0200

    Create HeaderIncludes.tsx

---
+++ pages/components/HeaderIncludes.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Head from 'next/head';
+
+const HeaderIncludes = () => {
+  return (
+    <Head>
+        <title>Simple To Do App</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="shortcut icon" href="favicon.png" type="image/x-icon"></link>
+    </Head>
+  )
+}
+export default HeaderIncludes
